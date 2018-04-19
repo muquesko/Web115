9d8d683a870073d4a422a146c625fe2a5685fa8c
Muquesko Toksis <35928173+muquesko@users.noreply.github.com>
2018-04-19 16:23:43 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,118 +1,12 @@
-"use strict";
+var imageList=["assets/amethyst.jpg", "assets/rocky.jpg",
+              "assets/roxie.jpg", "assets/sam.jpg", 
+               "assets/snowtail.jpg", "assets/tarantella.jpg"]
+function change(imageChange){
+currentImageIndex=imageChange;
+document.getElementById("rollover").src=imageList[imageChange];
+}
+var currentImageIndex=0;
 
-function changeToAmethyst() {
-	document.getElementById("jenkins-main-pic").src = "assets/amethyst.jpg";
-}
 
-function changeToRocky() {
-	document.getElementById("jenkins-main-pic").src = "assets/rocky.jpg";
-}
 
-function changeToRoxie() {
-	document.getElementById("jenkins-main-pic").src = "assets/roxie.jpg";
-}
 
-function changeToSam() {
-	document.getElementById("jenkins-main-pic").src = "assets/sam.jpg";
-}
-
-function changeToSnowtail() {
-	document.getElementById("jenkins-main-pic").src = "assets/snowtail.jpg";
-}
-
-function changeToTerry() {
-	document.getElementById("jenkins-main-pic").src = "assets/tarantella.jpg";
-}
-
-function changeToJackie() {
-	document.getElementById("jenkins-main-pic").src = "assets/jackie.jpg";
-}
-
-function changeToKaizer() {
-	document.getElementById("jenkins-main-pic").src = "assets/kaizer.jpg";
-}
-
-function changeToNick() {
-	document.getElementById("jenkins-main-pic").src = "assets/nick.jpg";
-}
-function changeToMari() {
-	document.getElementById("jenkins-main-pic").src = "assets/mari.jpg";
-}
-
-function changeToJacob() {
-	document.getElementById("understand-main-pic").src = "assets/jacob.jpg";
-}
-
-function changeToKayla() {
-	document.getElementById("understand-main-pic").src = "assets/kayla.jpg";
-}
-
-function changeToPeter() {
-	document.getElementById("understand-main-pic").src = "assets/peter.jpg";
-}
-
-function changeToKittipong() {
-	document.getElementById("understand-main-pic").src = "assets/kittipong.jpg";
-}
-
-function changeToJacobKayla() {
-	document.getElementById("understand-main-pic").src = "assets/jacob-kayla.jpg";
-}
-
-function changeToJoke() {
-	document.getElementById("understand-main-pic").src = "assets/joke.jpg";
-}
-
-function changeToKitty() {
-	document.getElementById("understand-main-pic").src = "assets/kitty-g.jpg";
-}
-
-function changeToMeilin() {
-	document.getElementById("understand-main-pic").src = "assets/meilin.jpg";
-}
-
-function changeToMango() {
-	document.getElementById("understand-main-pic").src = "assets/mango.jpg";
-}
-function changeToNutmeg() {
-	document.getElementById("understand-main-pic").src = "assets/nutmeg.jpg";
-}
-
-function changeToBrothers() {
-	document.getElementById("space-main-pic").src = "assets/brothers.jpg";
-}
-
-function changeToComet() {
-	document.getElementById("space-main-pic").src = "assets/comet.jpg";
-}
-
-function changeToDeath() {
-	document.getElementById("space-main-pic").src = "assets/death.jpg";
-}
-
-function changeTDwarfPlanet() {
-	document.getElementById("space-main-pic").src = "assets/dwarf-planet.jpg";
-}
-
-function changeToEuropa() {
-	document.getElementById("space-main-pic").src = "assets/europa.jpg";
-}
-
-function changeToJovianPlanet() {
-	document.getElementById("space-main-pic").src = "assets/jovian-planet.jpg";
-}
-
-function changeToKale() {
-	document.getElementById("space-main-pic").src = "assets/kale.jpg";
-}
-
-function changeToMercury() {
-	document.getElementById("space-main-pic").src = "assets/mercury.jpg";
-}
-
-function changeToPolaris() {
-	document.getElementById("space-main-pic").src = "assets/polaris.jpg";
-}
-function changeToSun() {
-	document.getElementById("space-main-pic").src = "assets/sun.jpg";
-}
